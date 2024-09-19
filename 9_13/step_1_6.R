a0886b85c00e14b7595b2b8e17fe0bc6b67d16b1
Yixuan Huang <yhuan279@jh.edu>
2024-09-19 17:22:18 -0400

    Update step_1_6.R

--- 9_13/step_1_6.R
+++ 9_13/step_1_6.R
@@ -1,10 +1,10 @@
 # step 1.6
 # Define the file name for the PNG output
 png(filename = "~/qbb2024-answers/9_13/ex1_30x_cov.png", width = 800, height = 600)
-# Load the coverage data into R
+# 30x
 coverage_data <- read.table("~/qbb2024-answers/9_13/coverage_30x.txt", header = TRUE)
 
-# Total number of genome positions
+
 N <- 1e6
 
 
@@ -14,12 +14,12 @@ hist(coverage_data$X0, breaks = 70, probability = TRUE, main = "Coverage Histogr
 # Generate a sequence of coverage depths
 coverage_depths <- 0:max(coverage_data$X0)
 
-# Poisson distribution with lambda = 3
+# Poisson distribution 30
 lambda <- 30
 poisson_probs <- dpois(coverage_depths, lambda)
 lines(coverage_depths, poisson_probs, col = "red", lwd = 2, type = "b")
 
-# Normal distribution with mean = 3 and std dev = sqrt(3)
+# Normal distribution 30
 mean_normal <- 30
 sd_normal <- sqrt(30)
 normal_probs <- dnorm(coverage_depths, mean = mean_normal, sd = sd_normal)
@@ -28,14 +28,14 @@ lines(coverage_depths, normal_probs, col = "blue", lwd = 2, type = "b")
 legend("topright", legend = c("Poisson Distribution", "Normal Distribution"), 
        col = c("red", "blue"), lwd = 3)
 
-# Count the number of positions with 0x coverage
-zero_coverage <- sum(coverage_data$X0 == 0)
-
 # Calculate the percentage of the genome with 0x coverage
-percentage_zero_coverage <- (zero_coverage / N) * 100
 
+zero_coverage <- sum(coverage_data$X0 == 0)
+percentage_zero_coverage <- (zero_coverage / N) * 100
+percentage_zero_coverage
+
+
+# Poisson prediction
 poisson_zero_coverage_prob <- dpois(0, lambda)
 
-
-# Save the plot and close the device
 dev.off()
