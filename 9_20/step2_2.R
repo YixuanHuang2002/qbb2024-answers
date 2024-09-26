c01468a1bf0838cf19052119b5c8536a6f7c59a7
Yixuan Huang <yhuan279@jh.edu>
2024-09-26 18:42:17 -0400

    step2_2

--- 9_20/step2_2.R
+++ 9_20/step2_2.R
@@ -14,10 +14,9 @@ cCREs <- snpenrich %>% filter(Feature == "cCREs")
 other <- snpenrich %>% filter(Feature == "other")
 
 ggplot() +
-  geom_line(data=exons,aes(MAF,logEnrichment,color="Exons")) + #Graphs exons and adds "Exons" to the legend
-  geom_line(data=introns,aes(MAF,logEnrichment,color="Introns")) + #See above, but Introns
-  geom_line(data=cCREs,aes(MAF,logEnrichment,color="cCREs")) + #See above, but cCREs
-  geom_line(data=other,aes(MAF,logEnrichment,color="Other")) + #See above, but Other gene elements
-  scale_color_colorblind() + #Scales the line colors in a colorblind safe manner
-  labs(color="Legend",x="Minor Allele Frequency",y="Log2 of SNP Enrichment",title="SNP Enrichment vs. Minor Allele Frequency") + #Adds axis labels, legend, and title
-  ggsave(filename = "~/qbb2024-answers/9_20/snp_enrichments.pdf") #Saves the plot to a pdf, ignore the error, as it still saves just fine.
+  geom_line(data=exons,aes(MAF,logEnrichment,color="Exons")) + 
+  geom_line(data=introns,aes(MAF,logEnrichment,color="Introns")) + 
+  geom_line(data=cCREs,aes(MAF,logEnrichment,color="cCREs")) + 
+  geom_line(data=other,aes(MAF,logEnrichment,color="Other")) + 
+  labs(color="Legend",x="Minor Allele Frequency",y="SNP Enrichment",title="SNP Enrichment of Each Feature") + 
+  ggsave(filename = "~/qbb2024-answers/9_20/snp_enrichments.pdf") 
