29ddb494f9a4f9b5925963ad8c70a8960249ed1e
Yixuan Huang <yhuan279@jh.edu>
2024-09-27 18:19:54 -0400

    correction for 1.3 plot code

---
+++ 9_13/step_1_3.R
@@ -0,0 +1,44 @@
+
+setwd("/Users/cmdb/qbb2024-answers/9_13")
+
+# step 1.3
+
+# create a png file
+png("ex1_3x_cov.png", width = 800, height = 600)
+# fetch the data
+coverage_data <- scan("/Users/cmdb/qbb2024-answers/9_13/coverage_3x.txt")
+
+hist(coverage_data,
+     breaks = seq(-0.5, max(coverage_data) + 0.5, by = 1),
+     xlab = "Coverage",
+     ylab = "Frequency",
+     main = "Coverage Depth",
+     col = "white",
+     border = "black")
+
+# set the parameters
+coverage_values <- 0:max(coverage_data)
+genome_size <- length(coverage_data)
+# Poisson
+lambda <- 3
+# Normal
+normal_mean <- 3
+normal_stdv <- sqrt(3)
+
+# the probes & counts for poisson and normal distribution
+poisson_probs <- dpois(coverage_values, lambda)
+poisson_counts <- poisson_probs * genome_size
+normal_probs <- dnorm(coverage_values, normal_mean, normal_stdv)
+normal_counts <- normal_probs * genome_size
+
+# Poisson distribution
+lines(coverage_values, poisson_counts, col = "red", lwd = 2)
+# Normal distribution
+lines(coverage_values, normal_counts, col = "blue", lwd = 2)
+
+# legend
+legend("topright", legend = c("Poisson Distribution", "Normal Distribution"), 
+       col = c("red", "blue"), lwd = 2)
+
+dev.off()
+
